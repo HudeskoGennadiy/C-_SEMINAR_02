1fd92fef7b8746aec6d783df5e0a413dd7f91fef
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-27 15:43:07 +0300

    example (try catch) solved

--- C#_try_catch/Program.cs
+++ C#_try_catch/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Функция (try catch)
+
+Console.Write("Введите целое число: ");
+try
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(x * 10);
+    }
+
+catch
+{
+    Console.WriteLine("Нужно было вводить целое число");
+}
+
+
