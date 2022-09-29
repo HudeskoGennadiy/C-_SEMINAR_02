e2623c79c9e78e6207e2d15338d30f9f0fecce4e
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-29 17:07:47 +0300

    examle multiple_namber solved

--- Array_random_bigNamber/Program.cs
+++ Array_random_bigNamber/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка[10,99] и показывает наибольшую цифру числа
+
+    
+void CompareDigits ()  
+    {
+    int x = new Random().Next(10 , 100);
+    int x1 = x/10;
+    int x2 = x%10;
+
+    Console.WriteLine($"Сгенерированно случайное число {x} ");
+    if(x1 >x2)Console.WriteLine("Первое цифра больше");
+    else if (x2 >x1)Console.WriteLine("Вторая цифра больше");
+    else Console.WriteLine("Это палиндром");
+    }
+CompareDigits();
+CompareDigits();
